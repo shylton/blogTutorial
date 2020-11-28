a90d3ae520151ba65c1de69d5bdb18412bed1b10
shylton <mattas@oregonstate.edu>
2020-11-28 14:19:35 -0300

    added base template

--- blogTutorial/views.py
+++ blogTutorial/views.py
@@ -1,9 +1,28 @@
 from django.shortcuts import render
 
 
+posts = [
+    {
+        'author': 'Tommy',
+        'title': 'Post 1',
+        'content': 'hola soy Tommy guac<br>me gusta muy guac.',
+        'date_posted': '27 aug 2020',
+    },
+    {
+        'author': 'Johnny Cerrantes',
+        'title': 'Post 2',
+        'content': 'hola soy J Bones!',
+        'date_posted': '28 aug 2020',
+    },
+]
+
+
 def home(req):
-    return render(req, 'blogTutorial/home.html')
+    context = {
+        'posts': posts,
+    }
+    return render(req, 'blogTutorial/home.html', context=context)
 
 
 def about(req):
-    return render(req, 'blogTutorial/about.html')
+    return render(req, 'blogTutorial/about.html', {'title': 'sm About'})
