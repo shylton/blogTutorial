ecdd60f38b78aabb86a27405f8098963eeb30912
shylton <mattas@oregonstate.edu>
2020-11-28 16:16:04 -0300

    end of part 5: created Post model

--- blogTutorial/models.py
+++ blogTutorial/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+# ORM = Object Relational Mapper. Django uses objects to build db independent
+#    of the backend! ex, can develop with sqlite then use postgres no prob!
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
--- blogTutorial/views.py
+++ blogTutorial/views.py
@@ -1,25 +1,10 @@
 from django.shortcuts import render
-
-
-posts = [
-    {
-        'author': 'Tommy',
-        'title': 'Post 1',
-        'content': 'hola soy Tommy guac<br>me gusta muy guac.',
-        'date_posted': '27 aug 2020',
-    },
-    {
-        'author': 'Johnny Cerrantes',
-        'title': 'Post 2',
-        'content': 'hola soy J Bones!',
-        'date_posted': '28 aug 2020',
-    },
-]
+from .models import Post
 
 
 def home(req):
     context = {
-        'posts': posts,
+        'posts': Post.objects.all(),
     }
     return render(req, 'blogTutorial/home.html', context=context)
 
