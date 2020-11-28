b4e0a776424e90a2e7bf6656ce1e00f5e18eab71
shylton <mattas@oregonstate.edu>
2020-11-28 12:06:11 -0300

    end of part 1

--- blogTutorial/urls.py
+++ blogTutorial/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='blog_home'),
+    path('', views.home, name='blog_home'),  # host/blog
+    path('about/', views.about, name='blog_about'),  # host/blog/about
 ]
--- blogTutorial/views.py
+++ blogTutorial/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def home(req):
     return HttpResponse('<h1>Blog home by sm</h1>')
+
+
+def about(req):
+    return HttpResponse('<h1>Blog About page</h1>')
